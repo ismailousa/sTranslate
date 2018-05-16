0714d1428fe46906a1f48c0caf3cc9014e38122d
Mobil Trakya <mobiltrakya@Mobils-iMac.local>
2018-05-16 18:22:42 +0300

    Initial Commit

---
+++ sTranslate2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sTranslate2
+//
+//  Created by Mobil Trakya on 16/05/2018.
+//  Copyright © 2018 isma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

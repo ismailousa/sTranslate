7f249ef5ff5f33b61a097ba00453fc030ff7cb6f
Isma-ilou Sadou <ismailou.sa@gmail.com>
2018-05-17 17:44:13 +0300

    iMessage extension added

---
+++ iMessageExt/GoogleTranslate.swift
@@ -0,0 +1,37 @@
+//
+//  GoogleTranslate.swift
+//  iMessageExt
+//
+//  Created by Mobil Trakya on 16/05/2018.
+//  Copyright © 2018 isma. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import Alamofire_Synchronous
+import SwiftyJSON
+
+public class GoogleTranslate {
+    public init() {
+        
+    }
+    
+    public var apiKey = "AIzaSyDa0ewdKIrNo7nxG0DTlLUTtVdUhdRNHLo"
+    
+    func translate(message: String) -> String{
+        var translation = "Null"
+        let urlEncodedText = message.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        
+        let response = Alamofire.request("https://translation.googleapis.com/language/translate/v2?key=\(self.apiKey)&q=\(urlEncodedText ?? "")&source=tr&target=en&format=text", method: .post, encoding: JSONEncoding.default).responseJSON();
+        if let json = response.result.value{
+            let jsonResponse = JSON(json)
+            print(jsonResponse)
+            if jsonResponse["data"]["translations"][0]["translatedText"].string != nil
+            {
+                translation = (jsonResponse["data"]["translations"][0]["translatedText"].string ?? "Null")
+            }
+            
+        }
+        return translation
+    }
+} 
